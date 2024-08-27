b26d78fdde6290b272d2c97e52baca85ced16019
Roni-Boiz <2019cs155@stu.ucsc.lk>
2024-08-28 00:59:28 +0530

    fix format issues

--- locals.tf
+++ locals.tf
@@ -1,7 +1,7 @@
 locals {
-  region = "ap-south-1"
-  availability_zones   = ["ap-south-1a", "ap-south-1b"]
+  region             = "ap-south-1"
+  availability_zones = ["ap-south-1a", "ap-south-1b"]
 
-  bucket_name               = "terraform-github-gitlab-action-tf-state-backend"
-  dynamodb_table_name       = "terraform-state-locking-table"
+  bucket_name         = "terraform-github-gitlab-action-tf-state-backend"
+  dynamodb_table_name = "terraform-state-locking-table"
 }
