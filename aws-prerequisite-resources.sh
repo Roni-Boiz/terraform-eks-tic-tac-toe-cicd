d4a44e3fb3e99d9004af14473d46c4bc07800927
Roni-Boiz <2019cs155@stu.ucsc.lk>
2024-08-28 15:33:59 +0530

    update bash script

--- aws-prerequisite-resources.sh
+++ aws-prerequisite-resources.sh
@@ -9,13 +9,13 @@ REGION="ap-south-1"
 if aws s3api head-bucket --bucket "$BUCKET_NAME" &>/dev/null; then
   echo "S3 bucket $BUCKET_NAME already exists."
 else
-  echo "Creating S3 bucket $BUCKET_NAME..."
   aws s3api create-bucket --bucket "$BUCKET_NAME" --region "$REGION" --create-bucket-configuration LocationConstraint="$REGION"
   aws s3api put-bucket-policy --bucket terraform-github-gitlab-tf-state-backend --policy '{
     "Version": "2012-10-17",
     "Statement": [
       {
         "Effect": "Allow",
+        "Principal": "*",
         "Action": [
           "s3:DeleteBucket",
           "s3:ListBucket",
@@ -30,7 +30,6 @@ else
     ]
   }'
   aws s3api put-bucket-versioning --bucket "$BUCKET_NAME" --versioning-configuration Status=Enabled
-  aws s3api put-bucket-acl --bucket $BUCKET_NAME --acl private
   aws s3api put-bucket-encryption --bucket "$BUCKET_NAME" --server-side-encryption-configuration '{
     "Rules": [{
       "ApplyServerSideEncryptionByDefault": {
@@ -38,19 +37,18 @@ else
       }
     }]
   }'
-  echo "S3 bucket $BUCKET_NAME created with versioning and encryption."
+  aws s3api head-bucket --bucket "$BUCKET_NAME"
 fi
 
 # Check if DynamoDB table exists
 if aws dynamodb describe-table --table-name "$DYNAMODB_TABLE_NAME" --region "$REGION" &>/dev/null; then
   echo "DynamoDB table $DYNAMODB_TABLE_NAME already exists."
 else
-  echo "Creating DynamoDB table $DYNAMODB_TABLE_NAME..."
   aws dynamodb create-table \
     --table-name "$DYNAMODB_TABLE_NAME" \
-    --attribute-definitions AttributeName=LockID, AttributeType=S \
-    --key-schema AttributeName=LockID, KeyType=HASH \
+    --attribute-definitions AttributeName=LockID,AttributeType=S \
+    --key-schema AttributeName=LockID,KeyType=HASH \
     --billing-mode PAY_PER_REQUEST \
     --region "$REGION"
-  echo "DynamoDB table $DYNAMODB_TABLE_NAME created."
+  aws dynamodb describe-table --table-name "$DYNAMODB_TABLE_NAME" --region "$REGION"
 fi
