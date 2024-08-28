3c11eac499ea811aa62f754e4853e3b9eb775ce1
Roni-Boiz <2019cs155@stu.ucsc.lk>
2024-08-28 14:51:50 +0530

    remove tf-state

--- main.tf
+++ main.tf
@@ -1,9 +1,3 @@
-module "tf-state" {
-  source              = "./modules/tf-state"
-  bucket_name         = local.bucket_name
-  dynamodb_table_name = local.dynamodb_table_name
-}
-
 module "eks" {
   source = "./modules/eks"
 }
