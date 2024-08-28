8748a1f6be6a40cd4a50df405e8f3ba0cf9c7e19
Roni-Boiz <2019cs155@stu.ucsc.lk>
2024-08-28 13:51:26 +0530

    rename backend

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-github-gitlab-action-tf-state-backend"
+    bucket         = "terraform-github-gitlab-tf-state-backend"
     key            = "tf-infra/terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "terraform-state-locking-table"
